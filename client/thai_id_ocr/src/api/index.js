cd62efce58aa1e5eae722c7f2729bbd6083efc13
harshk89 <80413986+harshk89@users.noreply.github.com>
2023-12-26 14:44:41 +0530

    changed url of server to hosted server

--- client/thai_id_ocr/src/api/index.js
+++ client/thai_id_ocr/src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000';
+// const url = 'http://localhost:5000';
+const url = 'https://thai-id-ocr-rhhe.onrender.com';
 
 const API = axios.create({ baseURL: url });
 
